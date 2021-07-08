924af9f037f9a8373ad312fdcccbb4c86085d528
nvkolezneva <nvkolezneva@gmail.com>
2021-07-08 17:41:59 +0300

    DEV:add answer output in question table

--- backend/questions/admin.py
+++ backend/questions/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import GroupQuestion, QuestionWithScale, AnswerScale, QuestionWithOption, AnswerOption
+from django.forms import TextInput, Textarea
+from django.db import models
 # Register your models here.
 
+class AnswerScaleInline(admin.StackedInline):
+    """Ответы на вопросы со шкалой"""
+    model = AnswerScale
+    extra = 1
+
+class AnswerOptionInline(admin.StackedInline):
+    """Ответы на вопросы с вариантами"""
+    model = AnswerOption
+    extra = 1
 
 class GroupQuestionAdmin(admin.ModelAdmin):
     """Группа вопросов"""
@@ -27,6 +38,11 @@ class QuestionWithScaleAdmin(admin.ModelAdmin):
             'fields': ('question_text', 'question_group', 'question_number',)
         }),
     )
+    inlines=[AnswerScaleInline]
+    # formfield_overrides = {
+    #     models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
 
 
 class AnswerScaleAdmin(admin.ModelAdmin):
@@ -40,6 +56,11 @@ class AnswerScaleAdmin(admin.ModelAdmin):
             'fields': ('order_number', 'left_answer_text', 'right_answer_text', 'question',)
         }),
     )
+    # formfield_overrides = {
+    #     models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
+
 
 
 class QuestionWithOptionAdmin(admin.ModelAdmin):
@@ -53,6 +74,11 @@ class QuestionWithOptionAdmin(admin.ModelAdmin):
             'fields': ('question_text', 'question_group', 'question_number',)
         }),
     )
+    inlines=[AnswerOptionInline]
+    # formfield_overrides = {
+    #     models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
 
 
 class AnswerOptionAdmin(admin.ModelAdmin):
@@ -70,6 +96,10 @@ class AnswerOptionAdmin(admin.ModelAdmin):
                        'question',)
         }),
     )
+    # formfield_overrides = {
+    #     models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+    #     models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    # }
 
 
 admin.site.register(GroupQuestion, GroupQuestionAdmin)
