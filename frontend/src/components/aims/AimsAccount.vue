76191f151ac9fb306067974ed4ff9bead7f5a10a
nvkolezneva <nvkolezneva@gmail.com>
2021-07-15 00:39:41 +0300

    dev: add loader in aims components

--- frontend/src/components/aims/AimsAccount.vue
+++ frontend/src/components/aims/AimsAccount.vue
@@ -1,180 +1,184 @@
 <template>
-  <v-flex>
-    <h1 class="title mb-12 text-center mb-5 mt-5 mb-sm-10 mt-sm-10">Цели</h1>
-    <div
-      v-if="!isEdit & !this.$apollo.queries.user.loading"
-      class="d-flex justify-center"
-    >
-      <div class="background-aimsaccount rounded-lg pa-8">
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Я ищу партнера:</p></v-col
+  <v-layout>
+    <AppLoader v-if="this.$apollo.queries.user.loading"></AppLoader>
+    <v-flex v-if="!this.$apollo.queries.user.loading">
+      <h1 class="title mb-12 text-center mb-5 mt-5 mb-sm-10 mt-sm-10">Цели</h1>
+      <div v-if="!isEdit" class="d-flex justify-center">
+        <div class="background-aimsaccount rounded-lg pa-8">
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Я ищу партнера:</p></v-col
+            >
+            <v-col class="mobile-padding1">
+              <p v-if="user.partnerType == 'GM'" class="gray-text">
+                для гостевого брака
+              </p>
+              <p v-if="user.partnerType == 'FAM'" class="gray-text">
+                для создания семьи
+              </p>
+              <p v-if="user.partnerType == 'JFF'" class="gray-text">
+                для просто поболтать и вместе потусить
+              </p></v-col
+            >
+          </v-row>
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Я хочу встретить:</p></v-col
+            >
+            <v-col class="mobile-padding1">
+              <p v-if="user.purposeMeet == 'SAME'" class="gray-text">
+                такого, как я
+              </p>
+              <p v-if="user.purposeMeet == 'ANTI'" class="gray-text">
+                мою противоположность
+              </p>
+              <p v-if="user.purposeMeet == 'MATH'" class="gray-text">
+                выбор путем математического алгоритма
+              </p></v-col
+            >
+          </v-row>
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Моя история по ощущениям:</p></v-col
+            >
+            <v-col class="mobile-padding1"
+              ><img
+                :src="
+                  require('../../assets/img/history/' +
+                    user.numberFotoHistoryByFelling +
+                    '.jpg')
+                "
+                alt="history_6"
+                class="width-img-history"
+            /></v-col>
+          </v-row>
+          <v-btn
+            class="mt-8 my-button white--text"
+            color="colorOfSea"
+            block="block"
+            type="submit"
+            @click="onEdit()"
           >
-          <v-col class="mobile-padding1">
-            <p v-if="user.partnerType == 'GM'" class="gray-text">
-              для гостевого брака
-            </p>
-            <p v-if="user.partnerType == 'FAM'" class="gray-text">
-              для создания семьи
-            </p>
-            <p v-if="user.partnerType == 'JFF'" class="gray-text">
-              для просто поболтать и вместе потусить
-            </p></v-col
-          >
-        </v-row>
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Я хочу встретить:</p></v-col
-          >
-          <v-col class="mobile-padding1">
-            <p v-if="user.purposeMeet == 'SAME'" class="gray-text">
-              такого, как я
-            </p>
-            <p v-if="user.purposeMeet == 'ANTI'" class="gray-text">
-              мою противоположность
-            </p>
-            <p v-if="user.purposeMeet == 'MATH'" class="gray-text">
-              выбор путем математического алгоритма
-            </p></v-col
-          >
-        </v-row>
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Моя история по ощущениям:</p></v-col
-          >
-          <v-col class="mobile-padding1"
-            ><img
-              :src="
-                require('../../assets/img/history/' +
-                  user.numberFotoHistoryByFelling +
-                  '.jpg')
-              "
-              alt="history_6"
-              class="width-img-history"
-          /></v-col>
-        </v-row>
-        <v-btn
-          class="mt-8 my-button white--text"
-          color="colorOfSea"
-          block="block"
-          type="submit"
-          @click="onEdit()"
-        >
-          Изменить
-        </v-btn>
+            Изменить
+          </v-btn>
+        </div>
       </div>
-    </div>
-    <div v-if="isEdit" class="d-flex justify-center">
-      <div class="background-aimsaccount rounded-lg pa-8">
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Я ищу партнера:</p></v-col
+      <div v-if="isEdit" class="d-flex justify-center">
+        <div class="background-aimsaccount rounded-lg pa-8">
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Я ищу партнера:</p></v-col
+            >
+            <v-col>
+              <v-radio-group v-model="partner" class="dark-blue-text">
+                <v-radio value="GM">
+                  <template v-slot:label>
+                    <div class="dark-blue-text">
+                      для гостевого брака
+                    </div></template
+                  >
+                </v-radio>
+                <v-radio value="FAM">
+                  <template v-slot:label>
+                    <div class="dark-blue-text">
+                      для создания семьи
+                    </div></template
+                  >
+                </v-radio>
+                <v-radio value="JFF">
+                  <template v-slot:label
+                    ><div class="dark-blue-text">
+                      для просто поболтать и вместе потусить
+                    </div></template
+                  >
+                </v-radio>
+              </v-radio-group></v-col
+            >
+          </v-row>
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Я хочу встретить:</p></v-col
+            >
+            <v-col
+              ><v-radio-group v-model="wish" class="dark-blue-text">
+                <v-radio value="SAME">
+                  <template v-slot:label>
+                    <div class="dark-blue-text">такого, как я</div></template
+                  >
+                </v-radio>
+                <v-radio value="ANTI">
+                  <template v-slot:label>
+                    <div class="dark-blue-text">
+                      мою противоположность
+                    </div></template
+                  >
+                </v-radio>
+                <v-radio value="MATH">
+                  <template v-slot:label>
+                    <div class="dark-blue-text">
+                      выбор путем математического алгоритма
+                    </div></template
+                  >
+                </v-radio>
+              </v-radio-group></v-col
+            >
+          </v-row>
+          <v-row class="direction-block">
+            <v-col class="mobile-padding"
+              ><p class="dark-blue-text">Моя история по ощущениям:</p></v-col
+            >
+            <v-col
+              ><v-row>
+                <v-col
+                  v-for="n in 9"
+                  :key="n"
+                  class="d-flex child-flex col-history"
+                  cols="4"
+                >
+                  <v-img
+                    :src="require('../../assets/img/history/' + n + '.jpg')"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                    :class="{ blueborder: n == isBlueBorder }"
+                    @click="isBlueBorder = n"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="grey lighten-5"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-col> </v-row
+            ></v-col>
+          </v-row>
+          <v-btn
+            class="mt-8 my-button white--text"
+            color="colorOfSea"
+            block="block"
+            type="submit"
+            @click="onUpdateAims(isBlueBorder, wish, partner)"
           >
-          <v-col>
-            <v-radio-group v-model="partner" class="dark-blue-text">
-              <v-radio value="GM">
-                <template v-slot:label>
-                  <div class="dark-blue-text">
-                    для гостевого брака
-                  </div></template
-                >
-              </v-radio>
-              <v-radio value="FAM">
-                <template v-slot:label>
-                  <div class="dark-blue-text">для создания семьи</div></template
-                >
-              </v-radio>
-              <v-radio value="JFF">
-                <template v-slot:label
-                  ><div class="dark-blue-text">
-                    для просто поболтать и вместе потусить
-                  </div></template
-                >
-              </v-radio>
-            </v-radio-group></v-col
-          >
-        </v-row>
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Я хочу встретить:</p></v-col
-          >
-          <v-col
-            ><v-radio-group v-model="wish" class="dark-blue-text">
-              <v-radio value="SAME">
-                <template v-slot:label>
-                  <div class="dark-blue-text">такого, как я</div></template
-                >
-              </v-radio>
-              <v-radio value="ANTI">
-                <template v-slot:label>
-                  <div class="dark-blue-text">
-                    мою противоположность
-                  </div></template
-                >
-              </v-radio>
-              <v-radio value="MATH">
-                <template v-slot:label>
-                  <div class="dark-blue-text">
-                    выбор путем математического алгоритма
-                  </div></template
-                >
-              </v-radio>
-            </v-radio-group></v-col
-          >
-        </v-row>
-        <v-row class="direction-block">
-          <v-col class="mobile-padding"
-            ><p class="dark-blue-text">Моя история по ощущениям:</p></v-col
-          >
-          <v-col
-            ><v-row>
-              <v-col
-                v-for="n in 9"
-                :key="n"
-                class="d-flex child-flex col-history"
-                cols="4"
-              >
-                <v-img
-                  :src="require('../../assets/img/history/' + n + '.jpg')"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                  :class="{ blueborder: n == isBlueBorder }"
-                  @click="isBlueBorder = n"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </v-col> </v-row
-          ></v-col>
-        </v-row>
-        <v-btn
-          class="mt-8 my-button white--text"
-          color="colorOfSea"
-          block="block"
-          type="submit"
-          @click="onUpdateAims(isBlueBorder, wish, partner)"
-        >
-          Сохранить
-        </v-btn>
+            Сохранить
+          </v-btn>
+        </div>
       </div>
-    </div>
-  </v-flex>
+    </v-flex></v-layout
+  >
 </template>
 
 <script>
+import AppLoader from "@/components/global/AppLoader.vue";
 import { USER_AIMS, UPDATE_USER_AIMS } from "@/graphql/user_queries";
 export default {
   name: "AimsAccount",
+  components: { AppLoader },
   apollo: {
     user: {
       query: USER_AIMS,
--- frontend/src/components/aims/AimsSignUp.vue
+++ frontend/src/components/aims/AimsSignUp.vue
@@ -1,90 +1,99 @@
 <template>
-  <v-flex>
-    <h1 class="title mb-12 text-center mb-5 mt-5 mb-sm-10 mt-sm-10">
-      Выберите цель
-    </h1>
-    <div class="d-flex justify-center">
-      <div>
-        <v-radio-group v-model="partner" class="dark-blue-text">
-          <label class="pb-4">1. Я ищу партнёра:</label>
-          <v-radio value="GM">
-            <template v-slot:label>
-              <div class="dark-blue-text">для гостевого брака</div>
-            </template>
-          </v-radio>
-          <v-radio value="FAM">
-            <template v-slot:label>
-              <div class="dark-blue-text">для создания семьи</div>
-            </template>
-          </v-radio>
-          <v-radio value="JFF">
-            <template v-slot:label>
-              <div class="dark-blue-text">
-                для просто поболтать и вместе потусить
-              </div>
-            </template>
-          </v-radio>
-        </v-radio-group>
-        <v-radio-group v-model="wish" class="dark-blue-text">
-          <label class="pb-4">2. Я хочу встретить:</label>
-          <v-radio value="SAME">
-            <template v-slot:label>
-              <div class="dark-blue-text">такого, как я</div>
-            </template>
-          </v-radio>
-          <v-radio value="ANTI">
-            <template v-slot:label>
-              <div class="dark-blue-text">мою противоположность</div>
-            </template>
-          </v-radio>
-          <v-radio value="MATH">
-            <template v-slot:label>
-              <div class="dark-blue-text">
-                выбор путем математического алгоритма
-              </div>
-            </template>
-          </v-radio>
-        </v-radio-group>
-
-        <p class="dark-blue-text">3. Выберите свою историю по ощущениям:</p>
-        <v-row>
-          <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
-            <v-img
-              :src="require('../../assets/img/history/' + n + '.jpg')"
-              aspect-ratio="1"
-              class="grey lighten-2"
-              :class="{ blueborder: n == isBlueBorder }"
-              @click="isBlueBorder = n"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-row>
+  <v-layout>
+    <AppLoader v-if="this.$apollo.queries.loading"></AppLoader>
+    <v-flex v-if="!this.$apollo.queries.loading">
+      <h1 class="title mb-12 text-center mb-5 mt-5 mb-sm-10 mt-sm-10">
+        Выберите цель
+      </h1>
+      <div class="d-flex justify-center">
+        <div>
+          <v-radio-group v-model="partner" class="dark-blue-text">
+            <label class="pb-4">1. Я ищу партнёра:</label>
+            <v-radio value="GM">
+              <template v-slot:label>
+                <div class="dark-blue-text">для гостевого брака</div>
               </template>
-            </v-img>
-          </v-col>
-        </v-row>
-        <v-btn
-          class="mt-8 my-button white--text"
-          color="colorOfSea"
-          block="block"
-          type="submit"
-          @click="onUpdateAims(isBlueBorder, wish, partner)"
-        >
-          Дальше
-        </v-btn>
+            </v-radio>
+            <v-radio value="FAM">
+              <template v-slot:label>
+                <div class="dark-blue-text">для создания семьи</div>
+              </template>
+            </v-radio>
+            <v-radio value="JFF">
+              <template v-slot:label>
+                <div class="dark-blue-text">
+                  для просто поболтать и вместе потусить
+                </div>
+              </template>
+            </v-radio>
+          </v-radio-group>
+          <v-radio-group v-model="wish" class="dark-blue-text">
+            <label class="pb-4">2. Я хочу встретить:</label>
+            <v-radio value="SAME">
+              <template v-slot:label>
+                <div class="dark-blue-text">такого, как я</div>
+              </template>
+            </v-radio>
+            <v-radio value="ANTI">
+              <template v-slot:label>
+                <div class="dark-blue-text">мою противоположность</div>
+              </template>
+            </v-radio>
+            <v-radio value="MATH">
+              <template v-slot:label>
+                <div class="dark-blue-text">
+                  выбор путем математического алгоритма
+                </div>
+              </template>
+            </v-radio>
+          </v-radio-group>
+
+          <p class="dark-blue-text">3. Выберите свою историю по ощущениям:</p>
+          <v-row>
+            <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
+              <v-img
+                :src="require('../../assets/img/history/' + n + '.jpg')"
+                aspect-ratio="1"
+                class="grey lighten-2"
+                :class="{ blueborder: n == isBlueBorder }"
+                @click="isBlueBorder = n"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-col>
+          </v-row>
+          <v-btn
+            class="mt-8 my-button white--text"
+            color="colorOfSea"
+            block="block"
+            type="submit"
+            @click="onUpdateAims(isBlueBorder, wish, partner)"
+          >
+            Дальше
+          </v-btn>
+        </div>
       </div>
-    </div>
-  </v-flex>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import AppLoader from "@/components/global/AppLoader.vue";
 import { UPDATE_USER_AIMS } from "@/graphql/user_queries";
 export default {
   name: "AimsSignUp",
+  components: { AppLoader },
   data() {
     return {
       isBlueBorder: undefined,
