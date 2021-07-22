fe705e112ecd55f34498cee510b42dca4aa2a485
Nikita Glazkov <zitrnik@gmail.com>
2021-07-22 21:18:01 +0300

    DEV: update test results screen

--- frontend/src/components/test/TestResult.vue
+++ frontend/src/components/test/TestResult.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>TestResult</div>
+  <v-layout align-center="align-center" justify-center="justify-center">
+    <v-flex class="text-center width-window">
+      <h1 class="title mb-5 mt-5 mb-sm-10 mt-sm-10">Ваши результаты</h1>
+      <p>
+        {{ textResultTest }}
+      </p>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: "TestResult"
+  name: "TestResult",
+  data() {
+    return {
+      textResultTest:
+        "Вы смелы и решительны. Вы можете принимать решения в зависимости от ситуации: то завоевать весь мир, то бросить все дела и переключить внимание на что-то очень важное. Вы не боятесь общественного мнения, у Вас есть свое мнение на любой счет."
+    };
+  }
 };
 </script>
 
