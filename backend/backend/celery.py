38d3226da7d3edbf13441b85adf98a6a671569aa
nvkolezneva <nvkolezneva@gmail.com>
2021-08-06 20:41:04 +0300

    DEV:add tasks with algorithms in celery! It is work! Wow!

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -12,8 +12,24 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'say_hello': {
-        'task': 'accounts.tasks.hello_world',
-        'schedule': 10.0
+    # 'say_hello': {
+    #     'task': 'accounts.tasks.hello_world',
+    #     'schedule': 10.0
+    # },
+    'first_algorithm': {
+        'task': 'accounts.tasks.first_algorithm',
+        'schedule': 3600.0
+    },
+    'second_algorithm': {
+        'task': 'accounts.tasks.second_algorithm',
+        'schedule': 3600.0
+    },
+    'third_algorithm': {
+        'task': 'accounts.tasks.third_algorithm',
+        'schedule': 3600.0
+    },
+    'fourth_algorithm': {
+        'task': 'accounts.tasks.fourth_algorithm',
+        'schedule': 3600.0
     }
 }
