a941edad0c9a1a9acd45662a4cea5db2e6a23df2
Nikita Glazkov <zitrnik@gmail.com>
2021-08-21 23:00:23 +0300

    dev: add ability to pu emoji into message string by cursor position

--- frontend/src/components/chat/ChatMessageInput.vue
+++ frontend/src/components/chat/ChatMessageInput.vue
@@ -74,8 +74,27 @@ export default {
   },
   methods: {
     selectEmoji(emoji) {
-      console.log(emoji);
-      this.newMessage += emoji.data;
+      let input = this.$refs.messageField.$refs.input;
+      let curPos = input.selectionStart;
+      this.newMessage =
+        this.newMessage.slice(0, curPos) +
+        emoji.data +
+        this.newMessage.slice(curPos);
+      let selectionStart;
+      let selectionEnd;
+      selectionStart = selectionEnd = curPos + emoji.data.length;
+      if (input.setSelectionRange) {
+        console.log(input.setSelectionRange(selectionStart, selectionEnd + 2));
+        input.focus();
+        input.setSelectionRange(selectionStart, selectionEnd + 2);
+      } else if (input.createTextRange) {
+        var range = input.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", selectionEnd);
+        range.moveStart("character", selectionStart);
+        range.select();
+      }
+      this.closeEmojiPicker();
     },
     closeEmojiPicker() {
       this.emojiPickerShow = false;
